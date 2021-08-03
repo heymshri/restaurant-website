667a86cfe2fb727b7a10b077f3052ab144c3afd5
heymshri <74291015+heymshri@users.noreply.github.com>
2021-08-03 11:18:12 +0545

    Add files via upload

---
+++ www/partials-front/footer.php
@@ -0,0 +1,36 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-right">
+             <div class="logo">
+                <a href="<?php echo SITEURL;?>" title="Logo">
+                    <img src="images\restaurant-logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-right">
+            <p>&copy; Designed By <a href="#">Shrijana Shakya</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+
+
+</body>
+</html>
