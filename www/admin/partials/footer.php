6ac9c8c8a4a88285916db5cb23fff41e1a5e2421
heymshri <74291015+heymshri@users.noreply.github.com>
2021-08-03 10:57:30 +0545

    Add files via upload

---
+++ www/admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- footer section-->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">&copy;Designed By <a href="#">Shrijana Shakya</a></p>
+    </div>
+</div>
+
+</body>
+
+</html>
